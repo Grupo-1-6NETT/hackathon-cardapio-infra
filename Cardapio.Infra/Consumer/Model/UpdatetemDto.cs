d51bacb46c67d6cd37fe8fb8c03e9bc1211e8ac1
Diego Feijó <diego.feijo@hotmail.com>
2025-06-26 22:14:07 -0300

    fix: reparos

--- Cardapio.Infra/Consumer/Model/UpdatetemDto.cs
+++ Cardapio.Infra/Consumer/Model/UpdatetemDto.cs
@@ -9,7 +9,7 @@ public class UpdateItemDto
     public Guid Id { get; set; }
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
-    public decimal Preco { get; set; }
+    public decimal? Preco { get; set; }
     public bool? Disponivel { get; set; }
     public string? NomeCategoria { get; set; }    
 }
