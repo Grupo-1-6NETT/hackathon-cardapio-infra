25dffde229d008c3e0b707fb4a15e1d6eecb1e9a
Diego Feijó <diego.feijo@hotmail.com>
2025-06-28 16:10:06 -0300

    fix: ajustes pos testes

--- Cardapio.Infra/Infrastructure/Repository/ItemRepository.cs
+++ Cardapio.Infra/Infrastructure/Repository/ItemRepository.cs
@@ -1,37 +1,70 @@
-﻿using Core.Entities;
+﻿using Core.Dto;
+using Core.Entities;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repository;
 public class ItemRepository(AppDbContext context) : IItemRepository
 {
-    private DbSet<Item> _entitySet => context.Items;
-    private IQueryable<Item> _queryable => context.Items.AsNoTracking();
+    private DbSet<Item> EntitySet => context.Items;
+    private IQueryable<Item> Queryable => context.Items.AsNoTracking();
     
 
-    public async Task<Item> SelectAsync(Guid id) => await _queryable.SingleAsync(x => x.Id == id);
+    public async Task<Item?> SelectAsync(Guid id) => await Queryable.FirstOrDefaultAsync(x => x.Id == id);
 
-    public async Task<bool> DeleteAsync(Guid id)
+    public async Task DeleteAsync(Guid id)
     {
-        var item = await _entitySet.SingleAsync(x => x.Id == id);
-        _entitySet.Remove(item);        
-        return await context.SaveChangesAsync() > 0;
+        var item = Queryable
+            .Include(x => x.Categoria)
+            .FirstOrDefault(x => x.Id == id)
+            ?? throw new KeyNotFoundException(nameof(id));
+        
+        EntitySet.Remove(item);
+        await context.SaveChangesAsync();
+        
     }
 
     public async Task<Item> InsertAsync(Item entity)
     {
+        entity.Categoria = await context.Categorias.SingleAsync(x => x.Id == entity.Categoria.Id);
+
         entity.DataCriacao = DateTime.Now.ToUniversalTime();
         entity.DataAtualizacao = DateTime.Now.ToUniversalTime();
-        _entitySet.Add(entity);
-        await context.SaveChangesAsync();        
+
+        await EntitySet.AddAsync(entity);
+        await context.SaveChangesAsync();
+        
         return entity;
     }
 
-    public async Task<Item> UpdateAsync(Item entity)
+    public async Task<Item> UpdateAsync(ItemDto dto)
     {
-        entity.DataAtualizacao = DateTime.Now.ToUniversalTime();
-        _entitySet.Attach(entity);
+        var current = await EntitySet
+            .FirstOrDefaultAsync(x => x.Id == dto.Id)
+            ?? throw new KeyNotFoundException(nameof(dto.Id));
+
+        if (!string.IsNullOrEmpty(dto.Nome?.Trim()))
+            current.Nome = dto.Nome.Trim();
+
+        if(!string.IsNullOrEmpty(dto.Descricao?.Trim()))
+            current.Descricao = dto.Descricao.Trim();
+
+        if(dto.Preco != null && dto.Preco.Value > 0)
+            current.Preco = dto.Preco.Value;
+
+        if(dto.Disponivel != null)
+            current.Disponivel = dto.Disponivel.Value;
+
+        if (dto.CategoriaId != null && dto.CategoriaId.Value != Guid.Empty && dto.CategoriaId.Value != current.Categoria.Id)
+            current.Categoria = await context.Categorias
+                .FirstOrDefaultAsync(x => x.Id == dto.CategoriaId.Value)
+                ?? throw new KeyNotFoundException(nameof(dto.CategoriaId));
+        
+        current.DataAtualizacao = DateTime.Now.ToUniversalTime();
+        
+        EntitySet.Update(current);
         await context.SaveChangesAsync();
-        return entity;
+
+        return current;
     }
 }
